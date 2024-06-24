c01840f4d4f71d46b879f07f123de81c27fbdd40
PC <PC@DESKTOP-IL1GD0V>
2024-06-24 15:16:02 +0530

    Local Automation project commit to git repo

---
+++ Automation-Framework/src/main/java/JAVA/Automation_Framework/TestNG_Test.java
@@ -0,0 +1,14 @@
+package JAVA.Automation_Framework;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Test {
+
+	@Test
+	public void test() 
+	{
+		System.out.println("TestNG");
+	}
+	
+}
+

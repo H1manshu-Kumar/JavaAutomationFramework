a57d38a54f1f35859ecedbed5283d4aa26fb7047
Himanshu Kumar <himanshu.kumar244@gmail.com>
2025-07-30 23:53:52 +0530

    Documentation Updated LandingPage.java :pencil:

--- src/main/java/pageObjects/LandingPage.java
+++ src/main/java/pageObjects/LandingPage.java
@@ -1,52 +1,59 @@
+// Declares this class belongs to the 'pageObjects' package
 package pageObjects;
 
+// Selenium imports for interacting with the browser and web elements
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import commonComponent.Common;
+import commonComponent.Common;  // Importing Common class for shared functionality
 
-public class LandingPage extends Common{
-	WebDriver driver;
+// Class representing the landing (login) page of the application
+public class LandingPage extends Common {
+	WebDriver driver;  // WebDriver instance to control the browser
 
+	// Constructor to initialize driver and web elements using PageFactory
 	public LandingPage(WebDriver driver) {
-		super(driver);
+		super(driver);  // Call to parent class constructor
 		this.driver = driver;
-		PageFactory.initElements(driver, this);
+		PageFactory.initElements(driver, this);  // Initialize elements marked with @FindBy
 	}
 
-	// WebElement userEmail = driver.findElement(By.id("userEmail"));
-
-	// PageFactory
+	// WebElement for email input field (replaces driver.findElement(By.id("userEmail")))
 	@FindBy(id = "userEmail")
 	private WebElement Email;
 
+	// WebElement for password input field
 	@FindBy(id = "userPassword")
 	private WebElement Password;
 
+	// WebElement for login button
 	@FindBy(id = "login")
 	private WebElement login;
-	
+
+	// WebElement for capturing error messages (like invalid credentials)
 	@FindBy(css = "[class*='flyInOut']")
 	private WebElement errorMessage;
 
-
+	// Method to navigate to the landing page URL
 	public void goTo() {
 		driver.get("https://rahulshettyacademy.com/client");
 	}
 
+	// Method to perform login with provided email and password
 	public ProductCatalogue loginApplication(String email, String password) {
-		Email.sendKeys(email);
-		Password.sendKeys(password);
-		login.click();
+		Email.sendKeys(email);        // Enter email
+		Password.sendKeys(password);  // Enter password
+		login.click();                // Click login button
+		// Navigate to and return the ProductCatalogue page object
 		ProductCatalogue productCatalogue = new ProductCatalogue(driver);
 		return productCatalogue;
 	}
-	
-	public String getErrorMessage() {
-		waitForElementToAppear(errorMessage);
-		return errorMessage.getText();
-	}
 
+	// Method to retrieve error message displayed on invalid login
+	public String getErrorMessage() {
+		waitForElementToAppear(errorMessage);  // Wait for error message to become visible
+		return errorMessage.getText();         // Return the error text
+	}
 }

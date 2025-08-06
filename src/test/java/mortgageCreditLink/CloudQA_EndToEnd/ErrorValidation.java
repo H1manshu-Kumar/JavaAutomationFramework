dc75d723dd180a2f5a93b2387a95c2e46bcff61e
Himanshu Kumar <himanshu.kumar244@gmail.com>
2025-08-06 09:19:46 +0530

    documentation updated ErrorValidation.java :pencil:

--- src/test/java/mortgageCreditLink/CloudQA_EndToEnd/ErrorValidation.java
+++ src/test/java/mortgageCreditLink/CloudQA_EndToEnd/ErrorValidation.java
@@ -5,43 +5,54 @@ import java.util.List;
 
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.IRetryAnalyzer;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.sun.net.httpserver.Authenticator.Retry;
 
 import mortgageCreditLink.BaseTest.BaseTest;
 import pageObjects.CartPage;
 import pageObjects.ProductCatalogue;
 
+// This class contains test methods to perform negative/error validation scenarios 
 public class ErrorValidation extends BaseTest {
+
+	// Expected product name and country for test data
 	String prodname = "ADIDAS ORIGINAL";
 	String country = "INDIA";
+
 	ExtentReports extent;
 
+	// Test to validate login error message when credentials are invalid
 	@Test(groups = { "ErrorHandling" }, retryAnalyzer = mortgageCreditLink.BaseTest.Retry.class)
 	public void LoginErrorValidation() {
-		ProductCatalogue productCatalogue = landingPage.loginApplication("test@gmail.com",
-				"Password@1234");
+		// Attempt login with incorrect credentials
+		ProductCatalogue productCatalogue = landingPage.loginApplication("test@gmail.com", "Password@1234");
+		
+		// Verify the actual error message (intentionally failing assertion for demonstration)
 		Assert.assertEquals("Login Successfully", landingPage.getErrorMessage());
 	}
 
+	// Test to validate error scenario when incorrect product is asserted in the cart
 	@Test
 	public void ProductErrorValidation() throws IOException, InterruptedException {
 
-		String prodname = "ADIDAS ORIGINAL";
-		String country = "INDIA";
-		ProductCatalogue productCatalogue = landingPage.loginApplication("test@gmail.com",
-				"Password@1234");
+		// Login with test credentials
+		ProductCatalogue productCatalogue = landingPage.loginApplication("test@gmail.com", "Password@1234");
 
+		// Get the list of all products
 		List<WebElement> productList = productCatalogue.getProductList();
+
+		// Add a specific product to cart
 		productCatalogue.addProductToCard(prodname);
+
+		// Navigate to the cart page
 		CartPage cartPage = productCatalogue.goToCartPage();
 
+		// Verify that an incorrect product (intentionally wrong name) is not present in the cart
 		Boolean match = cartPage.VerifyProductDisplay("Invalid Products");
-		Assert.assertFalse(match);
 
+		// Assert that the incorrect product is NOT found in the cart
+		Assert.assertFalse(match);
 	}
 }
